2476a5185a8d4f9d4ba35b2242ba4d6c6bf0b3de
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-06-02 00:24:59 +0600

    project initialized

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="min-h-screen flex justify-center items-center">
+        <h1 className="underline font-bold text-3xl">Hello World</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
