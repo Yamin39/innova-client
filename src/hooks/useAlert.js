d93c45466649e255be4e2189cfdeec2500842daf
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-06-02 05:30:23 +0600

    added custom hook: useAlert

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,34 @@
+import { Flip, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const useAlert = () => {
+  const successAlert = (message) =>
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Flip,
+    });
+
+  const errorAlert = (message) =>
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Flip,
+    });
+
+  return { successAlert, errorAlert };
+};
+
+export default useAlert;
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar/Navbar";
+import { Flip, ToastContainer } from "react-toastify";
 
 const Root = () => {
   return (
     <div className="max-w-[1440px] w-10/12 mx-auto font-dm-sans">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Flip}
+      />
     </div>
   );
 };
