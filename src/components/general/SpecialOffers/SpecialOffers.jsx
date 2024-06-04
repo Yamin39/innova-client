a044841ada1451c5848f3b77b6e1e7ead8463130
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-06-05 02:22:44 +0600

    updated special offers section

--- src/components/general/SpecialOffers/SpecialOffers.jsx
+++ src/components/general/SpecialOffers/SpecialOffers.jsx
@@ -3,6 +3,20 @@ import offerImg2 from "../../../assets/images/offer2.png";
 import offerImg3 from "../../../assets/images/offer3.jpeg";
 
 const SpecialOffers = () => {
+  const offers = [
+    {
+      id: 1,
+      img: offerImg1,
+    },
+    {
+      id: 2,
+      img: offerImg2,
+    },
+    {
+      id: 3,
+      img: offerImg3,
+    },
+  ];
   return (
     <div className="my-12 md:my-24">
       <div className="text-center mb-6">
@@ -10,30 +24,17 @@ const SpecialOffers = () => {
         <p className="sm:max-w-[25rem] mx-auto text-gray-500 mt-2">Exclusive Deals for Memorable Stays - Book Now and Save Big!</p>
       </div>
       <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <div
-          className="rounded-md flex overflow-hidden cursor-pointer flex-col justify-between"
-          style={{ boxShadow: "0px 0px 20px 8px rgba(59, 59, 59, 0.123)" }}
-        >
-          <div className="w-full h-[14rem] sm:h-[18.75rem] md:h-[22vw] lg:h-[11rem] xl:h-[12.5rem] 2xl:h-[18.75rem]">
-            <img className="size-full hover:scale-105 duration-300 rounded-md" src={offerImg1} />
+        {offers?.map((offer) => (
+          <div
+            key={offer.id}
+            className="rounded-md flex overflow-hidden cursor-pointer flex-col justify-between"
+            style={{ boxShadow: "0px 0px 20px 8px rgba(59, 59, 59, 0.123)" }}
+          >
+            <div className="w-full h-[14.5rem] sm:h-[20.75rem] md:h-[22vw] lg:h-[13rem] xl:h-[18.75rem]">
+              <img className="size-full hover:scale-105 duration-300 rounded-md" src={offer.img} />
+            </div>
           </div>
-        </div>
-        <div
-          className="rounded-md flex overflow-hidden cursor-pointer flex-col justify-between"
-          style={{ boxShadow: "0px 0px 20px 8px rgba(59, 59, 59, 0.123)" }}
-        >
-          <div className="w-full h-[14rem] sm:h-[18.75rem] md:h-[22vw] lg:h-[11rem] xl:h-[12.5rem] 2xl:h-[18.75rem]">
-            <img className="size-full hover:scale-105 duration-300 rounded-md" src={offerImg2} />
-          </div>
-        </div>
-        <div
-          className="rounded-md flex overflow-hidden cursor-pointer flex-col justify-between"
-          style={{ boxShadow: "0px 0px 20px 8px rgba(59, 59, 59, 0.123)" }}
-        >
-          <div className="w-full h-[14rem] sm:h-[18.75rem] md:h-[22vw] lg:h-[11rem] xl:h-[12.5rem] 2xl:h-[18.75rem]">
-            <img className="size-full hover:scale-105 duration-300 rounded-md" src={offerImg3} />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
